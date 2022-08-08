52e2348d6cddc153d704851b3e3866dfca382b5c
DkVZ <github@dkvz.eu>
2022-08-08 20:10:11 +0200

    Prepare generic code for event bus

---
+++ EventBus/EventBus/IIntegrationEventHandler.cs
@@ -0,0 +1,11 @@
+namespace EventBus;
+
+public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+    where TIntegrationEvent : IntegrationEvent
+{
+  Task Handle(TIntegrationEvent @event);
+}
+
+public interface IIntegrationEventHandler
+{
+}
